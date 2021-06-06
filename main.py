a07fe4b689f09d8767dfc38db7f15e56a209217f
Andrey <al9999things@gmail.com>
2021-06-06 11:31:17 -0400

    Downloads

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import requests
 import sys
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) < 2:
         print("Usage: haha no")
         exit(-1)
@@ -11,8 +11,21 @@ if __name__ == "__main__":
             print("I need to know what I am supposed to install!")
             exit(1)
         try:
-            installreq = json.loads(requests.get("https://api.spiget.org/v2/search/resources/" + sys.argv[2]).content)
+            installreq = json.loads(requests.get("https://api.spiget.org/v2/search/resources/" + sys.argv[2] + "?field=name&size=1").content)
         except:
             print("Error occured while fetching the API.")
             exit(1)
-        print(installreq) # for debug only. TODO: remove
+        downloadLink = installreq[0]["file"]["url"]
+        name = installreq[0]["name"]
+        print(downloadLink) # for debug only. TODO: remove
+        
+        with open(name + ".jar", "wb") as f:
+            requests.get("https://spigot.com/" + downloadLink)
+        
+        if len(installreq) != 1:
+            print("Could not find package {0}".format(sys.argv[2]))
+            exit(1)
+
+
+if __name__ == "__main__":
+    main()
